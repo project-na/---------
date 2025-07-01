f05e8fc90c4238a9665422316bcc8c91d40cf546
project-na <hoyam832015@gmail.com>
2025-07-01 11:49:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Array المصفوفة
+const langs = ["العربية" , "الانجليزية" , "الفرنسية"];
+// A1
+langs[0] = "الايطالية";
+langs.push("العربية")
+langs.pop()
+console.log(langs);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
